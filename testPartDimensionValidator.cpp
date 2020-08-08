8d13fed1bd5e565c09318cff82f5d42062130d2b
viswes31 <viswes31@gmail.com>
2020-08-08 19:37:28 +0530

    Adds main for gtest UT

--- testPartDimensionValidator.cpp
+++ testPartDimensionValidator.cpp
@@ -11,6 +11,11 @@ TEST(PartDimensionValidator, ValidInput)
 TEST(PartDimensionValidator, InvalidInput)
 {
 	float threshold = 0.5;
-	PartDimensionValidator(threshold);
-	ASSERT_EQ(true, validateParameter(0.6));
+	PartDimensionValidator partDimensionValidator(threshold);
+	ASSERT_EQ(true, partDimensionValidator.validateParameter(0.6));
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
